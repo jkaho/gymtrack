7e8a838a33444663e94fe998d9cfbc4d617fae74
jkaho <72959522+jkaho@users.noreply.github.com>
2021-03-12 12:26:54 +1100

    Create schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS blogger;
+CREATE DATABASE blogger;
+
+USE blogger;
+
+SELECT * FROM users;
+SELECT * FROM classes;
+SELECT * FROM userClasses;
+SELECT * FROM classReviews;
+SELECT * FROM instructorReviews;
