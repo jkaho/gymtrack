9cd4309b3dd2c179107a9546080dec80f80913a3
jkaho <72959522+jkaho@users.noreply.github.com>
2021-03-15 19:44:05 +1100

    Fix sequelize datatype syntax error: INT -> INTEGER

--- models/classes.js
+++ models/classes.js
@@ -21,7 +21,7 @@ module.exports = function(sequelize, DataTypes) {
         allowNull: false
       },
       price: {
-        type: DataTypes.INT,
+        type: DataTypes.INTEGER,
         allowNull: false
       }
     },
