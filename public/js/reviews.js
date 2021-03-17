cd0675bcb77bae8bc3a5a7b3b70894dcd60ea1c9
jkaho <72959522+jkaho@users.noreply.github.com>
2021-03-18 02:45:41 +1100

    Create modal for adding instructor reviews

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -30,32 +30,38 @@ $(document).ready(() => {
     }
   });
 
+  let rating;
   $(".rate-star").on("mouseover", function() {
     if (this.id === "star-one") {
+      rating = 1;
       this.setAttribute("style", "color: black");
       $("#star-two").css("color", "rgb(218, 210, 210)");
       $("#star-three").css("color", "rgb(218, 210, 210)");
       $("#star-four").css("color", "rgb(218, 210, 210)");
       $("#star-five").css("color", "rgb(218, 210, 210)");
     } else if (this.id === "star-two") {
+      rating = 2;
       $("#star-one").css("color", "black");
       this.setAttribute("style", "color: black");
       $("#star-three").css("color", "rgb(218, 210, 210)");
       $("#star-four").css("color", "rgb(218, 210, 210)");
       $("#star-five").css("color", "rgb(218, 210, 210)");
     } else if (this.id === "star-three") {
+      rating = 3;
       $("#star-one").css("color", "black");
       $("#star-two").css("color", "black");
       this.setAttribute("style", "color: black");
       $("#star-four").css("color", "rgb(218, 210, 210)");
       $("#star-five").css("color", "rgb(218, 210, 210)");
     } else if (this.id === "star-four") {
+      rating = 4;
       $("#star-one").css("color", "black");
       $("#star-two").css("color", "black");
       $("#star-three").css("color", "black");
       this.setAttribute("style", "color: black");
       $("#star-five").css("color", "rgb(218, 210, 210)");
     } else {
+      rating = 5;
       $("#star-one").css("color", "black");
       $("#star-two").css("color", "black");
       $("#star-three").css("color", "black");
@@ -70,12 +76,20 @@ $(document).ready(() => {
     });
   });
 
-  $(".exit-review-add").on("click", () => {
+  $("#add-class-review-link").on("click", () => {
+    $("#class-modal-bg").css("display", "block");
+  });
+
+  $("#exit-class-review").on("click", () => {
     $("#class-modal-bg").css("display", "none");
   });
 
-  $("#add-class-review-link").on("click", () => {
-    $("#class-modal-bg").css("display", "block");
+  $("#add-instructor-review-link").on("click", () => {
+    $("#instructor-modal-bg").css("display", "block");
+  });
+
+  $("#exit-instructor-review").on("click", () => {
+    $("#instructor-modal-bg").css("display", "none");
   });
 
   //   $(".add-review").on("click", () => {
