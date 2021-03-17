be1bcc57bc1ca55144cde8df806f6969ca5d86fa
jkaho <72959522+jkaho@users.noreply.github.com>
2021-03-17 21:39:37 +1100

    Edit /profile route -> code structure errors

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -229,20 +229,21 @@ module.exports = function(app) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/profile");
+    } else {
+      res.sendFile(path.join(__dirname, "../public/signup.html"));
     }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
   app.get("/login", (req, res) => {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/profile");
+    } else {
+      res.sendFile(path.join(__dirname, "../public/login.html"));
     }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
   });
   app.get("/logout", (req, res) => {
     req.logout();
     res.redirect("/");
-    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
@@ -253,20 +254,24 @@ module.exports = function(app) {
     } else {
       loggedIn = false;
     }
-    let userType;
-    let dateJoined;
-    let classDate;
-    let classInstructor;
-    const instructorClasses = [];
-    const memberClasses = [];
     db.user
       .findAll({
         include: [db.classes],
         where: { id: req.user.id }
       })
       .then(results => {
+        let userType;
+        let classDate;
+        let classInstructor;
+        const instructorClasses = [];
+        const memberClasses = [];
+        const firstName = results[0].dataValues.firstName;
+        const lastName = results[0].dataValues.lastName;
+        const email = results[0].dataValues.email;
         rawJoinDate = results[0].dataValues.createdAt;
-        dateJoined = moment(rawJoinDate).format("dddd, MMMM Do, yyyy, h:mma");
+        const dateJoined = moment(rawJoinDate).format(
+          "dddd, MMMM Do, yyyy, h:mma"
+        );
         if (results[0].dataValues.instructor === true) {
           userType = "Instructor";
           db.classes
@@ -276,62 +281,62 @@ module.exports = function(app) {
               }
             })
             .then(results => {
-              console.log(results);
+              if (results.length > 0) {
+                results.forEach(result => {
+                  rawClassDate = result.dataValues.startTime;
+                  classDate = moment(rawClassDate).format(
+                    "dddd, MMMM Do, h:mma"
+                  );
+                  const instructorClass = {
+                    classDate: classDate,
+                    name: result.dataValues.name,
+                    description: result.dataValues.description,
+                    price: result.dataValues.price
+                  };
+                  instructorClasses.push(instructorClass);
+                });
+                res.render("profile", {
+                  loggedIn: loggedIn,
+                  firstName: firstName,
+                  lastName: lastName,
+                  email: email,
+                  userType: userType,
+                  dateJoined: dateJoined,
+                  instructorClasses: instructorClasses,
+                  instructor: true,
+                  hasClasses: true
+                });
+              } else {
+                res.render("profile", {
+                  loggedIn: loggedIn,
+                  firstName: firstName,
+                  lastName: lastName,
+                  email: email,
+                  userType: userType,
+                  dateJoined: dateJoined,
+                  instructor: true,
+                  hasClasses: false
+                });
+              }
             });
-          results.forEach(result => {
-            if (result.dataValues.classes.length > 0) {
-              result.dataValues.classes.forEach(item => {
-                rawClassDate = item.dataValues.startTime;
-                classDate = moment(rawClassDate).format("dddd, MMMM Do, h:mma");
-                const instructorClass = {
-                  classDate: classDate,
-                  name: item.dataValues.name,
-                  description: item.dataValues.description,
-                  price: item.dataValues.price
-                };
-                instructorClasses.push(instructorClass);
-              });
-              res.render("profile", {
-                loggedIn: loggedIn,
-                firstName: results[0].dataValues.firstName,
-                lastName: results[0].dataValues.lastName,
-                email: results[0].dataValues.email,
-                userType: userType,
-                dateJoined: dateJoined,
-                instructorClasses: instructorClasses,
-                instructor: true,
-                hasClasses: true
-              });
-            } else {
-              res.render("profile", {
-                loggedIn: loggedIn,
-                firstName: results[0].dataValues.firstName,
-                lastName: results[0].dataValues.lastName,
-                email: results[0].dataValues.email,
-                userType: userType,
-                dateJoined: dateJoined,
-                instructor: true,
-                hasClasses: false
-              });
-            }
-          });
         } else {
           userType = "Member";
           results.forEach(result => {
             if (result.dataValues.classes.length > 0) {
-              result.dataValues.classes.forEach(item => {
-                rawClassDate = item.dataValues.startTime;
+              const resultArr = result.dataValues.classes;
+              for (let i = 0; i < resultArr.length; i++) {
+                rawClassDate = resultArr[i].dataValues.startTime;
                 classDate = moment(rawClassDate).format("dddd, MMMM Do, h:mma");
                 const memberClass = {
                   classDate: classDate,
-                  name: item.dataValues.name,
-                  description: item.dataValues.description,
-                  price: item.dataValues.price
+                  name: resultArr[i].dataValues.name,
+                  description: resultArr[i].dataValues.description,
+                  price: resultArr[i].dataValues.price
                 };
                 db.user
                   .findOne({
                     where: {
-                      id: item.dataValues.instructorId
+                      id: resultArr[i].dataValues.instructorId
                     }
                   })
                   .then(result => {
@@ -341,19 +346,21 @@ module.exports = function(app) {
                       result.dataValues.lastName;
                     memberClass.classInstructor = classInstructor;
                     memberClasses.push(memberClass);
-                    res.render("profile", {
-                      loggedIn: loggedIn,
-                      firstName: results[0].dataValues.firstName,
-                      lastName: results[0].dataValues.lastName,
-                      email: results[0].dataValues.email,
-                      userType: userType,
-                      dateJoined: dateJoined,
-                      memberClasses: memberClasses,
-                      member: true,
-                      hasClasses: true
-                    });
+                    if (i === resultArr.length - 1) {
+                      res.render("profile", {
+                        loggedIn: loggedIn,
+                        firstName: results[0].dataValues.firstName,
+                        lastName: results[0].dataValues.lastName,
+                        email: results[0].dataValues.email,
+                        userType: userType,
+                        dateJoined: dateJoined,
+                        memberClasses: memberClasses,
+                        member: true,
+                        hasClasses: true
+                      });
+                    }
                   });
-              });
+              }
             } else {
               res.render("profile", {
                 loggedIn: loggedIn,
