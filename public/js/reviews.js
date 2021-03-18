870e49ba33979aaf1b03f62dce9fbeffc795f300
jkaho <72959522+jkaho@users.noreply.github.com>
2021-03-18 12:52:11 +1100

    Fix star rating functionality for hovering and clicking

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -31,119 +31,235 @@ $(document).ready(() => {
     }
   });
 
-  // Rating star functionality on hover and click
   let rating;
-  $(".rate-star").on("mouseover", function() {
-    if (this.id === "star-one") {
+  let isStarClicked = false;
+
+  // Rating star functionality on hover and click (for class reviews)
+  $(".rate-star-c").on("mouseover", function() {
+    if (this.id === "star-one-c") {
       this.setAttribute("style", "color: black");
-      $("#star-two").css("color", "rgb(218, 210, 210)");
-      $("#star-three").css("color", "rgb(218, 210, 210)");
-      $("#star-four").css("color", "rgb(218, 210, 210)");
-      $("#star-five").css("color", "rgb(218, 210, 210)");
-    } else if (this.id === "star-two") {
-      $("#star-one").css("color", "black");
+      $("#star-two-c").css("color", "rgb(218, 210, 210)");
+      $("#star-three-c").css("color", "rgb(218, 210, 210)");
+      $("#star-four-c").css("color", "rgb(218, 210, 210)");
+      $("#star-five-c").css("color", "rgb(218, 210, 210)");
+    } else if (this.id === "star-two-c") {
+      $("#star-one-c").css("color", "black");
       this.setAttribute("style", "color: black");
-      $("#star-three").css("color", "rgb(218, 210, 210)");
-      $("#star-four").css("color", "rgb(218, 210, 210)");
-      $("#star-five").css("color", "rgb(218, 210, 210)");
-    } else if (this.id === "star-three") {
-      $("#star-one").css("color", "black");
-      $("#star-two").css("color", "black");
+      $("#star-three-c").css("color", "rgb(218, 210, 210)");
+      $("#star-four-c").css("color", "rgb(218, 210, 210)");
+      $("#star-five-c").css("color", "rgb(218, 210, 210)");
+    } else if (this.id === "star-three-c") {
+      $("#star-one-c").css("color", "black");
+      $("#star-two-c").css("color", "black");
       this.setAttribute("style", "color: black");
-      $("#star-four").css("color", "rgb(218, 210, 210)");
-      $("#star-five").css("color", "rgb(218, 210, 210)");
-    } else if (this.id === "star-four") {
-      $("#star-one").css("color", "black");
-      $("#star-two").css("color", "black");
-      $("#star-three").css("color", "black");
+      $("#star-four-c").css("color", "rgb(218, 210, 210)");
+      $("#star-five-c").css("color", "rgb(218, 210, 210)");
+    } else if (this.id === "star-four-c") {
+      $("#star-one-c").css("color", "black");
+      $("#star-two-c").css("color", "black");
+      $("#star-three-c").css("color", "black");
       this.setAttribute("style", "color: black");
-      $("#star-five").css("color", "rgb(218, 210, 210)");
+      $("#star-five-c").css("color", "rgb(218, 210, 210)");
     } else {
-      $("#star-one").css("color", "black");
-      $("#star-two").css("color", "black");
-      $("#star-three").css("color", "black");
-      $("#star-four").css("color", "black");
+      $("#star-one-c").css("color", "black");
+      $("#star-two-c").css("color", "black");
+      $("#star-three-c").css("color", "black");
+      $("#star-four-c").css("color", "black");
       this.setAttribute("style", "color: black");
     }
   });
 
-  let isStarClicked = false;
-  $(".rate-star").on("click", event => {
+  $(".rate-star-c").on("click", event => {
     isStarClicked = true;
-    if (event.target.id === "star-one") {
+    if (event.target.id === "star-one-c") {
       rating = 1;
-      $("#star-one").css("color", "black");
-      $("#star-two").css("color", "rgb(218, 210, 210)");
-      $("#star-three").css("color", "rgb(218, 210, 210)");
-      $("#star-four").css("color", "rgb(218, 210, 210)");
-      $("#star-five").css("color", "rgb(218, 210, 210)");
-    } else if (event.target.id === "star-two") {
+      $("#star-one-c").css("color", "black");
+      $("#star-two-c").css("color", "rgb(218, 210, 210)");
+      $("#star-three-c").css("color", "rgb(218, 210, 210)");
+      $("#star-four-c").css("color", "rgb(218, 210, 210)");
+      $("#star-five-c").css("color", "rgb(218, 210, 210)");
+    } else if (event.target.id === "star-two-c") {
       rating = 2;
-      $("#star-one").css("color", "black");
-      $("#star-two").css("color", "black");
-      $("#star-three").css("color", "rgb(218, 210, 210)");
-      $("#star-four").css("color", "rgb(218, 210, 210)");
-      $("#star-five").css("color", "rgb(218, 210, 210)");
-    } else if (event.target.id === "star-three") {
+      $("#star-one-c").css("color", "black");
+      $("#star-two-c").css("color", "black");
+      $("#star-three-c").css("color", "rgb(218, 210, 210)");
+      $("#star-four-c").css("color", "rgb(218, 210, 210)");
+      $("#star-five-c").css("color", "rgb(218, 210, 210)");
+    } else if (event.target.id === "star-three-c") {
       rating = 3;
-      $("#star-one").css("color", "black");
-      $("#star-two").css("color", "black");
-      $("#star-three").css("color", "black");
-      $("#star-four").css("color", "rgb(218, 210, 210)");
-      $("#star-five").css("color", "rgb(218, 210, 210)");
-    } else if (event.target.id === "star-four") {
+      $("#star-one-c").css("color", "black");
+      $("#star-two-c").css("color", "black");
+      $("#star-three-c").css("color", "black");
+      $("#star-four-c").css("color", "rgb(218, 210, 210)");
+      $("#star-five-c").css("color", "rgb(218, 210, 210)");
+    } else if (event.target.id === "star-four-c") {
       rating = 4;
-      $("#star-one").css("color", "black");
-      $("#star-two").css("color", "black");
-      $("#star-three").css("color", "black");
-      $("#star-four").css("color", "black");
-      $("#star-five").css("color", "rgb(218, 210, 210)");
+      $("#star-one-c").css("color", "black");
+      $("#star-two-c").css("color", "black");
+      $("#star-three-c").css("color", "black");
+      $("#star-four-c").css("color", "black");
+      $("#star-five-c").css("color", "rgb(218, 210, 210)");
     } else {
       rating = 5;
-      $("#star-one").css("color", "black");
-      $("#star-two").css("color", "black");
-      $("#star-three").css("color", "black");
-      $("#star-four").css("color", "black");
-      $("#star-five").css("color", "black");
+      $("#star-one-c").css("color", "black");
+      $("#star-two-c").css("color", "black");
+      $("#star-three-c").css("color", "black");
+      $("#star-four-c").css("color", "black");
+      $("#star-five-c").css("color", "black");
     }
   });
 
-  $(".rate-star").on("mouseout", () => {
+  $(".rate-star-c").on("mouseout", () => {
     if (!isStarClicked) {
-      $(".rate-star").each(function() {
+      $(".rate-star-c").each(function() {
         this.setAttribute("style", "color: rgb(218, 210, 210)");
       });
     } else {
       if (rating === 1) {
-        $("#star-one").css("color", "black");
-        $("#star-two").css("color", "rgb(218, 210, 210)");
-        $("#star-three").css("color", "rgb(218, 210, 210)");
-        $("#star-four").css("color", "rgb(218, 210, 210)");
-        $("#star-five").css("color", "rgb(218, 210, 210)");
+        $("#star-one-c").css("color", "black");
+        $("#star-two-c").css("color", "rgb(218, 210, 210)");
+        $("#star-three-c").css("color", "rgb(218, 210, 210)");
+        $("#star-four-c").css("color", "rgb(218, 210, 210)");
+        $("#star-five-c").css("color", "rgb(218, 210, 210)");
       } else if (rating === 2) {
-        $("#star-one").css("color", "black");
-        $("#star-two").css("color", "black");
-        $("#star-three").css("color", "rgb(218, 210, 210)");
-        $("#star-four").css("color", "rgb(218, 210, 210)");
-        $("#star-five").css("color", "rgb(218, 210, 210)");
+        $("#star-one-c").css("color", "black");
+        $("#star-two-c").css("color", "black");
+        $("#star-three-c").css("color", "rgb(218, 210, 210)");
+        $("#star-four-c").css("color", "rgb(218, 210, 210)");
+        $("#star-five-c").css("color", "rgb(218, 210, 210)");
       } else if (rating === 3) {
-        $("#star-one").css("color", "black");
-        $("#star-two").css("color", "black");
-        $("#star-three").css("color", "black");
-        $("#star-four").css("color", "rgb(218, 210, 210)");
-        $("#star-five").css("color", "rgb(218, 210, 210)");
+        $("#star-one-c").css("color", "black");
+        $("#star-two-c").css("color", "black");
+        $("#star-three-c").css("color", "black");
+        $("#star-four-c").css("color", "rgb(218, 210, 210)");
+        $("#star-five-c").css("color", "rgb(218, 210, 210)");
       } else if (rating === 4) {
-        $("#star-one").css("color", "black");
-        $("#star-two").css("color", "black");
-        $("#star-three").css("color", "black");
-        $("#star-four").css("color", "black");
-        $("#star-five").css("color", "rgb(218, 210, 210)");
+        $("#star-one-c").css("color", "black");
+        $("#star-two-c").css("color", "black");
+        $("#star-three-c").css("color", "black");
+        $("#star-four-c").css("color", "black");
+        $("#star-five-c").css("color", "rgb(218, 210, 210)");
       } else {
-        $("#star-one").css("color", "black");
-        $("#star-two").css("color", "black");
-        $("#star-three").css("color", "black");
-        $("#star-four").css("color", "black");
-        $("#star-five").css("color", "black");
+        $("#star-one-c").css("color", "black");
+        $("#star-two-c").css("color", "black");
+        $("#star-three-c").css("color", "black");
+        $("#star-four-c").css("color", "black");
+        $("#star-five-c").css("color", "black");
+      }
+    }
+  });
+
+  // Rating star functionality on hover and click (for instructor reviews)
+  $(".rate-star-i").on("mouseover", function() {
+    if (this.id === "star-one-i") {
+      this.setAttribute("style", "color: black");
+      $("#star-two-i").css("color", "rgb(218, 210, 210)");
+      $("#star-three-i").css("color", "rgb(218, 210, 210)");
+      $("#star-four-i").css("color", "rgb(218, 210, 210)");
+      $("#star-five-i").css("color", "rgb(218, 210, 210)");
+    } else if (this.id === "star-two-i") {
+      $("#star-one-i").css("color", "black");
+      this.setAttribute("style", "color: black");
+      $("#star-three-i").css("color", "rgb(218, 210, 210)");
+      $("#star-four-i").css("color", "rgb(218, 210, 210)");
+      $("#star-five-i").css("color", "rgb(218, 210, 210)");
+    } else if (this.id === "star-three-i") {
+      $("#star-one-i").css("color", "black");
+      $("#star-two-i").css("color", "black");
+      this.setAttribute("style", "color: black");
+      $("#star-four-i").css("color", "rgb(218, 210, 210)");
+      $("#star-five-i").css("color", "rgb(218, 210, 210)");
+    } else if (this.id === "star-four-i") {
+      $("#star-one-i").css("color", "black");
+      $("#star-two-i").css("color", "black");
+      $("#star-three-i").css("color", "black");
+      this.setAttribute("style", "color: black");
+      $("#star-five-i").css("color", "rgb(218, 210, 210)");
+    } else {
+      $("#star-one-i").css("color", "black");
+      $("#star-two-i").css("color", "black");
+      $("#star-three-i").css("color", "black");
+      $("#star-four-i").css("color", "black");
+      this.setAttribute("style", "color: black");
+    }
+  });
+
+  $(".rate-star-i").on("click", event => {
+    isStarClicked = true;
+    if (event.target.id === "star-one-i") {
+      rating = 1;
+      $("#star-one-i").css("color", "black");
+      $("#star-two-i").css("color", "rgb(218, 210, 210)");
+      $("#star-three-i").css("color", "rgb(218, 210, 210)");
+      $("#star-four-i").css("color", "rgb(218, 210, 210)");
+      $("#star-five-i").css("color", "rgb(218, 210, 210)");
+    } else if (event.target.id === "star-two-i") {
+      rating = 2;
+      $("#star-one-i").css("color", "black");
+      $("#star-two-i").css("color", "black");
+      $("#star-three-i").css("color", "rgb(218, 210, 210)");
+      $("#star-four-i").css("color", "rgb(218, 210, 210)");
+      $("#star-five-i").css("color", "rgb(218, 210, 210)");
+    } else if (event.target.id === "star-three-i") {
+      rating = 3;
+      $("#star-one-i").css("color", "black");
+      $("#star-two-i").css("color", "black");
+      $("#star-three-i").css("color", "black");
+      $("#star-four-i").css("color", "rgb(218, 210, 210)");
+      $("#star-five-i").css("color", "rgb(218, 210, 210)");
+    } else if (event.target.id === "star-four-i") {
+      rating = 4;
+      $("#star-one-i").css("color", "black");
+      $("#star-two-i").css("color", "black");
+      $("#star-three-i").css("color", "black");
+      $("#star-four-i").css("color", "black");
+      $("#star-five-i").css("color", "rgb(218, 210, 210)");
+    } else {
+      rating = 5;
+      $("#star-one-i").css("color", "black");
+      $("#star-two-i").css("color", "black");
+      $("#star-three-i").css("color", "black");
+      $("#star-four-i").css("color", "black");
+      $("#star-five-i").css("color", "black");
+    }
+  });
+
+  $(".rate-star-i").on("mouseout", () => {
+    if (!isStarClicked) {
+      $(".rate-star-i").each(function() {
+        this.setAttribute("style", "color: rgb(218, 210, 210)");
+      });
+    } else {
+      if (rating === 1) {
+        $("#star-one-i").css("color", "black");
+        $("#star-two-i").css("color", "rgb(218, 210, 210)");
+        $("#star-three-i").css("color", "rgb(218, 210, 210)");
+        $("#star-four-i").css("color", "rgb(218, 210, 210)");
+        $("#star-five-i").css("color", "rgb(218, 210, 210)");
+      } else if (rating === 2) {
+        $("#star-one-i").css("color", "black");
+        $("#star-two-i").css("color", "black");
+        $("#star-three-i").css("color", "rgb(218, 210, 210)");
+        $("#star-four-i").css("color", "rgb(218, 210, 210)");
+        $("#star-five-i").css("color", "rgb(218, 210, 210)");
+      } else if (rating === 3) {
+        $("#star-one-i").css("color", "black");
+        $("#star-two-i").css("color", "black");
+        $("#star-three-i").css("color", "black");
+        $("#star-four-i").css("color", "rgb(218, 210, 210)");
+        $("#star-five-i").css("color", "rgb(218, 210, 210)");
+      } else if (rating === 4) {
+        $("#star-one-i").css("color", "black");
+        $("#star-two-i").css("color", "black");
+        $("#star-three-i").css("color", "black");
+        $("#star-four-i").css("color", "black");
+        $("#star-five-i").css("color", "rgb(218, 210, 210)");
+      } else {
+        $("#star-one-i").css("color", "black");
+        $("#star-two-i").css("color", "black");
+        $("#star-three-i").css("color", "black");
+        $("#star-four-i").css("color", "black");
+        $("#star-five-i").css("color", "black");
       }
     }
   });
@@ -159,10 +275,12 @@ $(document).ready(() => {
 
   // Functionality for exiting review modals
   $("#exit-instructor-review").on("click", () => {
+    isStarClicked = false;
     $("#instructor-modal-bg").css("display", "none");
   });
 
   $("#exit-class-review").on("click", () => {
+    isStarClicked = false;
     $("#class-modal-bg").css("display", "none");
   });
 
@@ -173,6 +291,14 @@ $(document).ready(() => {
     console.log(rating);
     console.log($("#class-review-title-input").val());
     console.log($("#class-review-text-input").val());
+  });
+
+  $("#add-instructor-review").on("click", event => {
+    event.preventDefault();
+    console.log($("#instructor-reviews-list").val());
+    console.log(rating);
+    console.log($("#instructor-review-title-input").val());
+    console.log($("#instructor-review-text-input").val());
   });
   //   $(".add-review").on("click", () => {
   //     $.ajax({
