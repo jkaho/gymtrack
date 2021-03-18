e5d49bca1cb6e348a7f9ff172ca0bb67fabdd2c3
JoshSteward <73278132+JoshSteward@users.noreply.github.com>
2021-03-18 21:04:43 +1100

    Update classes-api-routes.js

--- routes/classes-api-routes.js
+++ routes/classes-api-routes.js
@@ -13,8 +13,8 @@ module.exports = function(app) {
   });
 
   // Route for adding new gym classes
-  app.post("/api/add_class", (req, res) => {
-    db.Classes.create({
+  app.post("/api/classes", (req, res) => {
+    db.classes.create({
       name: req.body.name,
       description: req.body.description,
       startTime: req.body.startTime,
@@ -24,4 +24,34 @@ module.exports = function(app) {
       res.status(401).json(err);
     });
   });
+
+    //route for editing classes 
+    app.put("/api/classes", (req,res) => {
+      db.classes.update(
+      {
+        name: req.body.name,
+        descrption: req.body.description,
+        startTime: req.body.startTime,
+        endTime: req.body.endTime,
+        adminId: req.instructor.id,
+      },
+      {
+        where:{
+          id: req.body.id,
+        },
+      }
+      ).then((dbclasses) => res.json(dbclasses))
+      .catch((err) => res.json(err));
+    });
+  
+    //route for deleting classes
+    app.delete("/api/classes/:id", (req, res) => {
+      //specify which class to delete 
+      db.classes.destroy({
+        where: {
+          id: req.params.id,
+        },
+      }).then((dbclasses) => res.json(dbclasses));
+    });
 };
+
