5d5f4228834375d24fe8b7df790ddb13a7c5fe6c
jkaho <72959522+jkaho@users.noreply.github.com>
2021-03-21 15:05:48 +1100

    Add else/if statements for rendering stars to classes page

--- public/js/classes.js
+++ public/js/classes.js
@@ -2,5 +2,30 @@ $(document).ready(() => {
   const avgRatingDiv = $(".avg-rating-div");
   avgRatingDiv.forEach(div => {
     const avgRating = div.attr("id").split("-")[3];
-  })
-};
+    if (avgRating > 0 && avgRating <= 1) {
+
+    } else if (avgRating > 0 && avgRating <= 0.5) {
+
+    } else if (avgRating > 0.5 && avgRating <= 1) {
+
+    } else if (avgRating > 1 && avgRating <= 0.5) {
+
+    } else if (avgRating > 0 && avgRating <= 0.5) {
+
+    } else if (avgRating > 0 && avgRating <= 0.5) {
+
+    } else if (avgRating > 0 && avgRating <= 0.5) {
+
+    } else if (avgRating > 0 && avgRating <= 0.5) {
+
+    } else if (avgRating > 0 && avgRating <= 0.5) {
+
+    } else if (avgRating > 0 && avgRating <= 0.5) {
+
+    } else if (avgRating > 0 && avgRating <= 0.5) {
+
+    } else if (avgRating > 0 && avgRating <= 0.5) {
+
+    }
+  });
+});
